64831f1fb0b1da3922eb517fb51eb9e026b1b017
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2020-11-04 16:55:01 +0300

    [2] Fix black screen
    * Set window rootViewController

--- SBBOL/AppDelegate.swift
+++ SBBOL/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         rootAssembly = RootAssembly(inputModel: RootInputModel(), delegate: self)
+        window?.rootViewController = rootAssembly?.contentViewController
+        window?.makeKeyAndVisible()
         return true
     }
 

58f022871d4c051048eb4b7f649fbf0c4b4088c9
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2020-11-05 17:43:18 +0300

    [3] Refactoring
    * Extract common functionality to APIService class

--- SBBOL/Services/API/TranslateAPIService/AzureTranslateAPIService.swift
+++ SBBOL/Services/API/TranslateAPIService/AzureTranslateAPIService.swift
@@ -8,13 +8,10 @@
 import Foundation
 
 ///  Translate service for Microsoft Azure
-final class AzureTranslateAPIService: TranslateAPIService {
-
-    private let session: URLSession
-    private let path = "https://api.cognitive.microsofttranslator.com/translate"
+final class AzureTranslateAPIService: APIService, TranslateAPIService {
 
     init(session: URLSession = APISettings.defaultSession) {
-        self.session = session
+        super.init(session: session, path: "https://api.cognitive.microsofttranslator.com/translate")
     }
 
     /// Text translation
@@ -23,42 +20,16 @@ final class AzureTranslateAPIService: TranslateAPIService {
     ///   - to: Destination language code
     ///   - completion: Translation or error
     func translate(text: String, to: String, completion: @escaping (Result<[AzureTranslateResponse], Error>) -> Void) {
-        var components = URLComponents(string: path)
-        components?.queryItems = [URLQueryItem(name: "to", value: to),
-                                  URLQueryItem(name: "api-version", value: "3.0")]
-        guard let url = components?.url else {
-            completion(.failure(APIError.path))
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setDefaultAzureHeaders(auth: true)
+        let queryItems = [URLQueryItem(name: "to", value: to),
+                          URLQueryItem(name: "api-version", value: "3.0")]
 
         let requestData = AzureTranslateRequestData(text: text)
         do {
-            request.httpBody = try JSONEncoder().encode([requestData]) // array not single element
+            let httpBody = try JSONEncoder().encode([requestData]) // array not single element
+            perform(httpMethod: "POST", httpBody: httpBody, auth: true, queryItems: queryItems, completion: completion)
         } catch {
             completion(.failure(APIError.encoding))
             return
         }
-
-        session.dataTask(with: request) { (data, response, error) in
-            guard error == nil else {
-                completion(.failure(APIError.service(error: error!)))
-                return
-            }
-            guard let response = response as? HTTPURLResponse,
-                  response.statusCode == 200,
-                  let data = data else {
-                completion(.failure(APIError.network))
-                return
-            }
-            guard let decoding = try? JSONDecoder().decode([AzureTranslateResponse].self, from: data) else {
-                completion(.failure(APIError.decoding))
-                return
-            }
-            completion(.success(decoding))
-        }.resume()
     }
 }
